a60b9a68977f0171589cd08af0ca18e051db0749
soraiemame <funasou46@gmail.com>
2025-11-30 15:50:17 +0900

    fix condition missing

--- src/pages/ItemCreate.tsx
+++ src/pages/ItemCreate.tsx
@@ -17,7 +17,7 @@ import {
 import { Input } from "../components/ui/input";
 import { Label } from "../components/ui/label";
 import { Textarea } from "../components/ui/textarea";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../components/ui/select" //
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../components/ui/select"
 import { X, Upload } from "lucide-react";
 
 export function ItemCreate() {
